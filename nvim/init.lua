1bd9e8cfc418643b1cd8f57c826b279dc20f928e
Shaimole <felixurlaub@googlemail.com>
2023-07-17 17:17:22 +0000

    add more treesitter parsers

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,7 +66,26 @@ lsp.setup()
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "python", "typescript" },
+  ensure_installed =     {  "bash",
+  			    "c",
+			    "html",
+			    "javascript",
+			    "json",
+			    "lua",
+			    "luadoc",
+			    "luap",
+			    "markdown",
+			    "markdown_inline",
+			    "python",
+			    "query",
+			    "regex",
+			    "tsx",
+			    "typescript",
+			    "vim",
+			    "vimdoc",
+			    "yaml",
+			    "rust",
+		},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
